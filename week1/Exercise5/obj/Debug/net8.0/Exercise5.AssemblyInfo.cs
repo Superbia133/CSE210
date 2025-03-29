5856f77b9b9f7cc0ca9f0dfe32e5ae7579ec92cc
Superbia133 <egriffeth@byupathway.edu>
2025-03-28 22:40:34 -0700

    update

--- week1/Exercise5/obj/Debug/net8.0/Exercise5.AssemblyInfo.cs
+++ week1/Exercise5/obj/Debug/net8.0/Exercise5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3baf724b649c5790e381891cdceb40641cfdf3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f490f585894049402f0711cb561f3d9e5c240a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
