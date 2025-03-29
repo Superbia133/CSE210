7e23c99899622ce2c7e740762ef1516c41d28a0e
Superbia133 <egriffeth@byupathway.edu>
2025-03-28 19:28:36 -0700

    youtube

--- week1/Exorcise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
+++ week1/Exorcise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e5e1efe6fa89ecbb3496280f6ecd40c6e77641")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3baf724b649c5790e381891cdceb40641cfdf3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
